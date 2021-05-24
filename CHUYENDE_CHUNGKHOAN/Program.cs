6a0a28748807be4c2304c7db2ebf6c450cb6bcea
abc <abc@example.com>
2021-05-24 09:35:35 +0700

    first commit

---
+++ CHUYENDE_CHUNGKHOAN/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CHUYENDE_CHUNGKHOAN
+{
+    static class Program
+    {
+
+
+        // tạo đối tượng kết nối Conn , kêt nối Database bằng mã lệnh
+        public static SqlConnection conn = new SqlConnection();
+        public static String connstr;
+        public static SqlDataAdapter da=null;
+
+        public static String servername = "VUONGDEV";
+        public static String username = "sa";
+        public static String password = "123";
+        public static String database = "CHUNGKHOAN";
+      
+
+
+
+        public static int KetNoi()
+        {
+            if (Program.conn != null && Program.conn.State == ConnectionState.Open)
+                Program.conn.Close();
+            try
+            {
+                Program.connstr = "Data Source=" + Program.servername + ";Initial Catalog=" +
+                      Program.database + ";User ID=" +
+                      Program.username + ";password=" + Program.password;
+                Program.conn.ConnectionString = Program.connstr;
+                Program.conn.Open();
+                return 1;
+            }
+
+            catch (Exception e)
+            {
+                MessageBox.Show("Lỗi kết nối cơ sở dữ liệu.\nBạn xem lại user name và password.\n " + e.Message, "", MessageBoxButtons.OK);
+                return 0;
+            }
+        }
+
+        //private void GetData()
+        //{
+        //    command.Notification = null;
+
+        //    SqlDependency dependency = new SqlDependency(command);
+        //    dependency.OnChange += dependency_OnChange;
+
+        //    DataTable dt = new DataTable();
+        //    dt.Load(command.ExecuteReader());
+        //    this.gcBANGGIATRUCTUYEN.DataSource = dt;
+
+        //If dataToWatch Is Nothing Then
+        //dataToWatch = New DataSet()
+        //End If
+        //}
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmMain());
+        }
+    }
+}
---
+++ CHUYENDE_CHUNGKHOAN/frmMAIN.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace CHUYENDE_CHUNGKHOAN
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //Kien tra form da load vao bo nho chua ?
+        private Form CheckExists(Type ftype)
+        {
+            foreach (Form f in this.MdiChildren)
+                if (f.GetType() == ftype)
+                    return f;
+            return null;
+        }
+
+
+        private void frmMAIN_Load(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void barBtn_DATLENH_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+       
+      
+        private void barBtnCHUNGKHOAN_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            Form frm = this.CheckExists(typeof(frmCHUNGKHOAN));
+            if (frm != null) frm.Activate();
+            else
+            {
+                frmCHUNGKHOAN f = new frmCHUNGKHOAN();
+                f.MdiParent = this;
+                f.Show();
+            }
+        }
+
+        private void barBtn_THOAT_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            DialogResult h = MessageBox.Show
+               ("Bạn có chắc muốn thoát không?", "Thoát chương trình", MessageBoxButtons.OKCancel);
+            if (h == DialogResult.OK)
+                Application.Exit();
+        }
+    }
+}
